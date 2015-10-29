b35f5fbe1a3ea963a0ff06145c6f4f28d1bc9106
Leonhardt Wille <wille@riverlabs.de>
2015-10-29 13:50:17 +0100

    Added ubuntu15 to DIST_NAME whitelist

--- src/aerospike/scripts/aerospike-client-c.sh
+++ src/aerospike/scripts/aerospike-client-c.sh
@@ -66,7 +66,7 @@ detect_linux()
         return 0
         ;;
 
-      "ubuntu12" | "ubuntu13" | "ubuntu14" | "linuxmint17" )
+      "ubuntu12" | "ubuntu13" | "ubuntu14" | "ubuntu15" | "linuxmint17" )
         echo "ubuntu12"  "deb"
         return 0
         ;;
